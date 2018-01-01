003b17a9ba176fc4db91963fddb738b2c1edf093
Alessandro Desantis <desa.alessandro@gmail.com>
2018-01-01 13:49:45 +0100

    Document Runner API

--- lib/pragma/migration/runner.rb
+++ lib/pragma/migration/runner.rb
@@ -2,21 +2,50 @@
 
 module Pragma
   module Migration
+    # Runners run the migrations of a {Bond} on requests/responses.
     class Runner
+      # @!attribute [r] bond
+      #   @return [Bond] the bond used by this runner
       attr_reader :bond
 
+      # Initializes the runner.
+      #
+      # @param bond [Bond] the bond to use
       def initialize(bond)
         @bond = bond
       end
 
+      # Returns the request this runner is operating on.
+      #
+      # This is just a shortcut for the bond's request.
+      #
+      # @return [Rack::Request]
+      #
+      # @see Bond#request
       def request
         bond.request
       end
 
+      # Returns the repository this runner is operating with.
+      #
+      # This is just a shortcut for the bond's repository.
+      #
+      # @return [Repository]
+      #
+      # @see Bond#repository
       def repository
         bond.repository
       end
 
+      # Runs all the migrations applying to the request upwards.
+      #
+      # Note that only the first migration is run with the original request: each migration after
+      # the first will be run with the request returned by the previous migration.
+      #
+      # @return [Rack::Request] the final request
+      #
+      # @see Bond#applying_migrations
+      # @see Base.up
       def run_upwards
         result = request
 
@@ -27,6 +56,17 @@ module Pragma
         result
       end
 
+      # Runs all the migrations applying to the request downwards.
+      #
+      # Note that only the first migration is run with the original response: each migration after
+      # the first will be run with the response returned by the previous migration.
+      #
+      # Also, when running downwards the migration's request is always the user's original request.
+      #
+      # @return [Rack::Response] the final response
+      #
+      # @see Bond#applying_migrations
+      # @see Base.down
       def run_downwards(response)
         result = response
 
